073a7e828a8966d35f393a9b180f479272e136e2
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2025-07-15 18:38:03 +0100

    stringify

--- main.ts
+++ main.ts
@@ -29,38 +29,47 @@ class Complex implements IComplex {
     }
 
     // a + bi + c + di = (a+c) + (b+d)i
-    static add(c1: Complex, c2: Complex) {
-        return new Complex(c1.real + c2.real, c1.imaginary + c2.imaginary)
+    static add(z: Complex, w: Complex) {
+        return new Complex(z.real + w.real, z.imaginary + w.imaginary)
     }
 
     // a + bi - (c + di) = (a-c) + (b-d)i
-    static sub(c1: Complex, c2: Complex) {
-        return new Complex(c1.real - c2.real, c1.imaginary - c2.imaginary)
+    static sub(z: Complex, w: Complex) {
+        return new Complex(z.real - w.real, z.imaginary - w.imaginary)
     }
 
     // (a + bi)(c + di) = (ac-bd) + (ad+bc)i 
-    static mul(c1: Complex, c2: Complex) {
-        const ac = c1.real * c2.real
-        const bd = c1.imaginary * c2.imaginary
-        const ad = c1.real * c2.imaginary
-        const bc = c1.imaginary * c2.real
+    static mul(z: Complex, w: Complex) {
+        const ac = z.real * w.real
+        const bd = z.imaginary * w.imaginary
+        const ad = z.real * w.imaginary
+        const bc = z.imaginary * w.real
         return new Complex(ac - bd, ad + bc)
     }
 
-    conjugate() { return new Complex(this.real, this.imaginary) }
+    conjugate() { return new Complex(this.real, -this.imaginary) }
 
     //(a+bi)/(c+di) = (a+bi)(c-di)/(c+di)(c-di)
     // (thanks wolframalpha for the solution I did not want to work this out myself)
     // = ((ac+bd) / (c^2+d^2)) + ((bc-ad) / (c^2+d^2))i
 
-    static div(c1: Complex, c2: Complex) {
-        const ac = c1.real * c2.real
-        const bd = c1.imaginary * c2.imaginary
-        const ad = c1.real * c2.imaginary
-        const bc = c1.imaginary * c2.real
-        const csq = c2.real ^ 2
-        const dsq = c2.imaginary ^ 2
+    static div(z: Complex, w: Complex) {
+        const ac = z.real * w.real
+        const bd = z.imaginary * w.imaginary
+        const ad = z.real * w.imaginary
+        const bc = z.imaginary * w.real
+        const csq = w.real ^ 2
+        const dsq = w.imaginary ^ 2
         return new Complex((ac + bd) / (csq + dsq), (bc - ad) / (csq + dsq))
+    }
 
+    stringify() {
+        if(this.imaginary === 0) return `${this.real}`
+        if (this.real === 0) return `${this.imaginary}i`
+        if(this.imaginary < 0) return `${this.real} - ${Math.abs(this.imaginary)}i`
+        return `${this.real} + ${this.imaginary}i`
     }
 }
+
+
+//* TEST 1: 
